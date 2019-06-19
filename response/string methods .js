b30adb73325238295454407c932a86971f64b5f6
Hanan Alharbi <hananalharbi@MacBook-Pro-alkhas-b-Hanan.local>
2019-06-19 16:39:58 +0300

    solved

---
+++ response/string methods .js
@@ -0,0 +1,25 @@
+//Method 1
+// Name: length
+//Description:The length property returns the length of a string:
+//Example using 
+it:var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var sln = txt.length;
+
+
+
+//Method 2
+
+//Name:indexOf
+//Description:The indexOf() method returns the index of (the position of) the first occurrence of a specified text in a string:
+//Example using it: 
+var str = "Please locate where 'locate' occurs!";
+var pos = str.indexOf("locate");
+
+
+//Method 3
+
+//Name: search
+//Description: The search() method searches a string for a specified value and returns the position of the match
+//Example using it:
+var str = "Please locate where 'locate' occurs!";
+var pos = str.search("locate");
